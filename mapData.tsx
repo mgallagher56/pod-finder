90abe8213f887f4df324e48c1ba854841f7b5a4d
Marc Gallagher-Reid <marc.gr@velocitypartners.com>
2021-03-18 01:00:57 +0000

    adde map and auto search

---
+++ mapData.tsx
@@ -0,0 +1,38 @@
+export const mapData: Array<object> = [
+    {
+        title: "Victoria Square",
+        address: "1 Victoria Square, Belfast BT1 4QG",
+        latitude: 54.60039782715394,
+        longitude: -5.925175951576578,
+    },
+    {
+        title: "Connswater Shopping Centre",
+        address: "Bloomfield Ave, A20, Belfast BT5 5LP",
+        latitude: 54.597712879419525, 
+        longitude: -5.889642048684822,
+    },
+    {
+        title: "Westfield Stratford City",
+        address: "Montfichet Rd, London E20 1EJ",
+        latitude: 51.545477186142634,
+        longitude:  -0.005236123420659279,
+    },
+    {
+        title: "Old Spitalfields Market",
+        address: "16 Commercial St, Spitalfields, London E1 6EW",
+        latitude: 51.519743079808364, 
+        longitude: -0.07556394672969176,
+    },
+    {
+        title: "Sainsbury's Pimlico",
+        address: "99 Wilton Rd, Pimlico, London SW1V 1DT",
+        latitude: 51.495148595772655, 
+        longitude:-0.14081815479323395,
+    },
+    {
+        title: "Sainsbury's Birmingham",
+        address: "17 Union St, Birmingham B2 4SJ",
+        latitude: 52.48163043139932,
+        longitude: -1.8960794706111912,
+    },
+]
--- screens/FindAPodScreen.tsx
+++ screens/FindAPodScreen.tsx
@@ -1,21 +1,77 @@
 import * as React from 'react';
-import { StyleSheet, Dimensions } from 'react-native';
-import { TextInput } from 'react-native-gesture-handler';
-import MapView from 'react-native-maps';
+import { StyleSheet, Dimensions, Button } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+// import { GOOGLE_API_KEY } from "react-native-dotenv";
 
-
-import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
+import { mapData } from '../mapData';
 
 export default function FindAPodScreen() {
+    const _map = React.useRef(null);
+    const latitudeDeltaInitial = 0.25;
+    const longitudeDeltaInitial = 0.25;
+    const [region, setRegion] = React.useState({
+        latitudeDelta: latitudeDeltaInitial,
+        longitudeDelta: longitudeDeltaInitial,
+        latitude: 54.5977151700977,
+        longitude: -5.9299603236191
+    });
+
+    let searchText: any;
+
     return (
-        <View style={styles.container}>
-            <MapView style={styles.map} />
-            <View style={styles.textContainer}>
-                <EditScreenInfo path="/screens/FindAPodScreen.tsx" subtitle="Enter location to search" />
-                <TextInput style={styles.input} placeholder={'location'} />
+        <View>
+            <MapView
+                style={styles.map}
+                region={region}>
+                {mapData.map((data: any) => {
+                    return <Marker
+                        title={data.title}
+                        description={data.address}
+                        coordinate={{ latitude: data.latitude, longitude: data.longitude }}
+                        image={require('../assets/images/logo-map-icon.png')}
+                        key={data.title}
+                    />
+                })
+                }
+            </MapView>
+            <View style={{ position: 'absolute', top: 0, width: '100%' }}>
+                <GooglePlacesAutocomplete
+                    ref={(c) => (searchText = c)}
+                    placeholder={'Enter Location'}
+                    minLength={2}
+                    fetchDetails={true}
+                    styles={{
+                        textInputContainer: styles.textContainer,
+                        textInput: {
+                            height: 38,
+                            color: '#5d5d5d',
+                            fontSize: 16,
+                            width: 100,
+                        },
+                        predefinedPlacesDescription: {
+                            color: '#1faadb',
+                        },
+                    }}
+                    onPress={(data, details = null) => {
+                        // 'details' is provided when fetchDetails = true
+                        // searchText.setAddressText(details?.formatted_address);
+
+                        setRegion(prevState => ({ 
+                            ...prevState,
+                            latitude: details.geometry.location.lat,
+                            longitude: details.geometry.location.lng,
+                        }))
+                    }}
+                    query={{
+                        key: 'AIzaSyAykd9G34qFbb7rMR0rrhJ_bd-PbNh31PY',
+                        language: 'en',
+                        components: 'country:uk',
+                    }}
+                />
             </View>
-        </View>
+        </View >
     );
 }
 
@@ -24,13 +80,10 @@ const styles = StyleSheet.create({
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
-        height: '10%'
     },
     textContainer: {
         width: '100%',
         marginVertical: 10,
-        alignItems: 'center',
-        justifyContent: 'center',
     },
     title: {
         fontSize: 20,
@@ -48,6 +101,7 @@ const styles = StyleSheet.create({
     },
     map: {
         width: Dimensions.get('window').width,
-        height: '90%',
+        position: 'relative',
+        height: '100%'
     },
 });
